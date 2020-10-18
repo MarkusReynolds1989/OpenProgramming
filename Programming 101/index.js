4c2696368757394ddb9421ea06775c8808045f39
Markus <markusreynolds1989@gmail.com>
2020-10-17 21:05:41 -0500

    Started programming 101

---
+++ Programming 101/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello, World!");
+console.log(3 + 3);
+let x;
+x = 3 + 3;
+let y;
+console.log(y);
---
+++ Programming 101/index.ts
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+console.log(3 + 3);
+let x;
+x = 3 + 3;
+let y;
+console.log(y);
